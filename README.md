c44d0e28b0942e9b347b472d7ba717b5e153454f
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-05-31 15:42:53 +0800

    Add GET response handler

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# urbanfarming
+# EG3301R Urban Farming SSC-334
+
+### Setup Guide
+1. Make sure you have npm installed on your computer. You can install node by going to https://nodejs.org and download from there. Check that you have installed correctly by typing `node-v` and `npm -v`
+2. Clone the repository to your local computer
+3. Navigate to the directory
+4. Run `npm install`
+5. Run `npm start`. The console should print out `Server running at http://localhost:3000`
--- index.js
+++ index.js
@@ -1,18 +1,28 @@
+
 const express = require('express'),
      http = require('http');
 
 const hostname = 'localhost';
 const port = 3000;
+const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'))
 
-app.use((req, res, next) => {
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+app.use(express.json({ limit: '5mb' }));
 
-});
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    next();
+})
+
+var sample = require('./public/sample.json');
+
+app.get('/dishes', (req,res,next) => {
+    res.json(sample);
+})
 
 const server = http.createServer(app);
 
