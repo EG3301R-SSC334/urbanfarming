62b7fb721bd37c31ab3f15010e17c04398225a6e
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-06-14 21:50:27 +0800

    Change port

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.use(morgan('dev'));
 app.use(express.json({ limit: '5mb' }));
 app.use('/plants', plantRouter);
 
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running...`)
 });
