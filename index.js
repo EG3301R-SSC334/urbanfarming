fee370e03511522bfbeb4ffbff02429494d9a38b
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-06-14 17:29:22 +0800

    Connect express with mongodb

--- index.js
+++ index.js
@@ -1,41 +1,19 @@
 const express = require('express'),
      http = require('http');
-const mongoose = require('mongoose')
-const Dishes = require('./models/sample')
-const url = 'mongodb://localhost:27017/conFusion';
-const connect = mongoose.connect(url);
-connect.then((db) => {
-
-    console.log('Connected correctly to server');
-
-    Dishes.create({
-            name: 'Uthappizza',
-            description: 'test'
-        })
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-
-});
-
+     
 const hostname = 'localhost';
 const port = 3000;
 const morgan = require('morgan');
 const app = express();
 const dishRouter = require('./routes/dishRouter');
+const mongoose = require('mongoose')
+const Dishes = require('./models/sample')
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'))
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,29 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 
 const dishRouter = express.Router();
+const Dishes = require('../models/sample');
 
 dishRouter.use(bodyParser.json());
 const sample = require('../public/sample.json');
 
 dishRouter.route('/')
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    next();
-})
 .get((req,res,next) => {
-    res.json(sample);
+    Dishes.find({})
+    .then((dishes) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dishes);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .post((req, res, next) => {
-    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+    Dishes.create(req.body)
+    .then((dish) => {
+        console.log('Dish Created ', dish);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .put((req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /dishes');
 })
 .delete((req, res, next) => {
-    res.end('Deleting all dishes');
+    Dishes.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));    
 });
 
 module.exports = dishRouter;
