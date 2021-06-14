07d5098025d4ec5466a0192321c5186785da80f9
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-06-14 16:08:40 +0800

    Change from driver to mongoose

--- index.js
+++ index.js
@@ -1,51 +1,35 @@
 const express = require('express'),
      http = require('http');
+const mongoose = require('mongoose')
+const Dishes = require('./models/sample')
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+connect.then((db) => {
 
-// Connect to MongoDB
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-const url = 'mongodb://localhost:27017'
-const dbname = 'conFusion';
-const operations = require('./utils/operations')
+    console.log('Connected correctly to server');
 
-MongoClient.connect(url).then((client) => {
+    Dishes.create({
+            name: 'Uthappizza',
+            description: 'test'
+        })
+        .then((dish) => {
+            console.log(dish);
 
-  console.log('Connected correctly to server');
-  const db = client.db(dbname);
+            return Dishes.find({});
+        })
+        .then((dishes) => {
+            console.log(dishes);
 
-  dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
-      "dishes")
-      .then((result) => {
-          console.log("Insert Document:\n", result.ops);
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 
-          return dboper.findDocuments(db, "dishes");
-      })
-      .then((docs) => {
-          console.log("Found Documents:\n", docs);
-
-          return dboper.updateDocument(db, { name: "Vadonut" },
-                  { description: "Updated Test" }, "dishes");
-
-      })
-      .then((result) => {
-          console.log("Updated Document:\n", result.result);
-
-          return dboper.findDocuments(db, "dishes");
-      })
-      .then((docs) => {
-          console.log("Found Updated Documents:\n", docs);
-                          
-          return db.dropCollection("dishes");
-      })
-      .then((result) => {
-          console.log("Dropped Collection: ", result);
-
-          return client.close();
-      })
-      .catch((err) => console.log(err));
-
-})
-.catch((err) => console.log(err));
+});
 
 const hostname = 'localhost';
 const port = 3000;
