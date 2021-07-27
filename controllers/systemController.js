8457eaf7b16fca3329921a1e124a14226aa858e6
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-07-27 21:13:11 +0800

    Modify controller

--- controllers/systemController.js
+++ controllers/systemController.js
@@ -1,6 +1,6 @@
 import { Systems } from '../models/systemSchema.js'
 
-export const getAllSystems = (req, res, next) => {
+export const getSystemData = (req, res, next) => {
     Systems.find({})
     .then((system) => {
         res.statusCode = 200;
@@ -8,4 +8,38 @@ export const getAllSystems = (req, res, next) => {
         res.json(system);
     }, (err) => next(err))
     .catch((err) => next(err));
+}
+
+export const createNewSystem = (req, res, next) => {
+    Systems.create(req.body)
+    .then((system) => {
+        console.log('system Created ', system);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(system);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+}
+
+export const updateSystemData = (req, res, next) => {
+    Systems.findByIdAndUpdate(req.body._id, {
+        $push: { temperature: req.body.temperature }
+    }, { new: true })
+    .then((system) => {
+        console.log('System updated ', system);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(system);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+}
+
+export const deleteSystemData = (req, res, next) => {
+    Systems.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));    
 }
--- routes/systemRouter.js
+++ routes/systemRouter.js
@@ -1,41 +1,13 @@
 import express from 'express';
 import { Systems } from '../models/systemSchema.js';
-import { getAllSystems } from '../controllers/systemController.js';
+import { createNewSystem, deleteSystemData, getSystemData, updateSystemData } from '../controllers/systemController.js';
 
 const systemRouter = express.Router();
 
 systemRouter.route('/')
-.get(getAllSystems)
-.post((req, res, next) => {
-    Systems.create(req.body)
-    .then((system) => {
-        console.log('system Created ', system);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(system);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-})
-.put((req, res, next) => {
-    Systems.findByIdAndUpdate(req.body._id, {
-        $push: { temperature: req.body.temperature }
-    }, { new: true })
-    .then((system) => {
-        console.log('system Created ', system);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(system);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-})
-.delete((req, res, next) => {
-    Systems.remove({})
-    .then((resp) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(resp);
-    }, (err) => next(err))
-    .catch((err) => next(err));    
-});
+.get(getSystemData)
+.post(createNewSystem)
+.put(updateSystemData)
+.delete(deleteSystemData);
 
 export default systemRouter;
