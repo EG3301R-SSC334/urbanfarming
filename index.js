d305a7dcc537a3a6be0a0ef76beb48733e58409f
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-09-13 12:15:39 +0800

    revert index.js back

--- index.js
+++ index.js
@@ -6,14 +6,7 @@ import path from 'path';
 import logger from 'morgan';
 import systemRouter from './routes/systemRouter.js';
 import userRouter from './routes/userRouter.js';
-import authRouter from './routes/authRouter.js';
-import session from 'express-session';
-import passport from 'passport';
-import MongoStore from 'connect-mongo';
-import { ensureAuth, ensureGuest } from './utils/sociallogin.js';
 
-// https://tomanagle.medium.com/google-oauth-with-node-js-4bff90180fe6
-// Tutorial for non-passport related use
 const __dirname = path.resolve();
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
@@ -26,6 +19,10 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json({ limit: '5mb' }));
 
+// Declare Routes
+app.use('/users', userRouter);
+app.use('/systems', systemRouter);
+
 const connect = mongoose.connect(
     process.env.DB_URL, {
     useNewUrlParser: true,
@@ -37,12 +34,6 @@ connect.then((db) => {
     console.log("Connected correctly to server");
 }, (err) => { console.log(err); });
 
-// Declare Routes
-app.use('/users', userRouter);
-app.use('/systems', systemRouter);
-app.use('/', authRouter);
-
-
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running...`)
+    console.log(`Server running...`)
 });
