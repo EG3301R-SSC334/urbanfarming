270cca0851016883e581529a56eb56ab33fe2e8d
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-09-23 20:39:00 +0800

    Add oauth2 client

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,12 @@
 import {Users} from '../models/userSchema.js';
 import { getToken } from '../utils/passport.js';
+import { google } from 'googleapis';
+
+const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URI
+);
 
 export async function googleLogin (req, res , next) {
     const input = req.body.token;
