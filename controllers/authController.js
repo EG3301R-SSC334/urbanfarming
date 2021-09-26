c7fa74a59518e6759d5b923fb7a26b61c10c15d7
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-09-26 18:33:39 +0800

    Added console log

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import {Users} from '../models/userSchema.js';
 import { getToken } from '../utils/passport.js';
 import { google } from 'googleapis';
+import axios from 'axios';
 
 const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
@@ -9,18 +10,18 @@ const oauth2Client = new google.auth.OAuth2(
 );
 
 export async function googleLogin (req, res , next) {
-    console.log("req: " + req)
-    console.log("res: " + res)
-    console.log("req body: " + Object.keys(req.body).length)
-    console.log("req headers: " + JSON.stringify(req.headers))
-    console.log("req body username: " + req.body.username)
-    console.log("req body email: " + req.body.email)
-    const input = req.body.token;
-    console.log("input: " + input)
+    // console.log("req: " + req)
+    // console.log("res: " + res)
+    // console.log("req body: " + Object.keys(req.body).length)
+    // console.log("req headers: " + JSON.stringify(req.headers))
+    // console.log("req body username: " + req.body.username)
+    // console.log("req body email: " + req.body.email)
+    const body = req.body;
+    // console.log("input: \n" + input)
     let googleUser;
     try {
-        googleUser = await getGoogleUser({ code: input });
-        console.log("googleuser: " + googleUser)
+        googleUser = await getGoogleUser(body.id_token, body.access_token);
+        console.log("============================================GOOGLEUSER: " + googleUser)
     
         const user = await Users.findOne({ email: String(googleUser.id) });
         console.log("user: " + user)
@@ -41,10 +42,13 @@ export async function googleLogin (req, res , next) {
         } else {
             try {
                 const createNewUser = await Users.create(req.body);
+                console.log("============================================CREATE NEW USER============================================")
+                console.log(createNewUser)
                 if (createNewUser != null) {
                     res.statusCode = 200;
                     res.setHeader('Content-Type', 'application/json');
                     createNewUser.bearerToken = token; // add the bearer token
+                    console.log(createNewUser);
                     res.json(createNewUser);
                 } 
             } catch (err) {
@@ -60,16 +64,21 @@ export async function googleLogin (req, res , next) {
     }
   }
 
-async function getGoogleUser({ code }) {
-    const { tokens } = await oauth2Client.getToken(code);
+async function getGoogleUser(id_token, access_token) {
+    console.log("============================================TOKENS ARE NOT OBTAINED============================================")
+    // console.log(code)
+    // const { tokens } = await oauth2Client.getToken(code);
+    console.log("============================================TOKENS ARE OBTAINED ALREADY============================================")
+    // tokens.access_token = code
+    // print(access_token)
 
     // Fetch the user's profile with the access token and bearer
     const googleUser = await axios
         .get(
-        `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${tokens.access_token}`,
+        `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`,
         {
             headers: {
-            Authorization: `Bearer ${tokens.id_token}`,
+            Authorization: `Bearer ${id_token}`,
             },
         },
         )
@@ -78,7 +87,10 @@ async function getGoogleUser({ code }) {
             res.statusCode = 400;
             res.send(err);
         });
+        console.log("============================================GOOGLE USER============================================")
 
+        console.log(googleUser)
+        console.log("============================================TOKENS SENT============================================")
     return googleUser;
 }
   
